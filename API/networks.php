96ad579dae5455dc8c1eedd669c4eeda6c638fcd
Tim Falken <timfalken@hotmail.com>
2016-05-31 21:53:38 +0200

    Fixed information given in users and networks

--- API/networks.php
+++ API/networks.php
@@ -3,6 +3,42 @@
 include "database.php";
 
 if(isset($_GET["id"]))
-	echo json_encode($networks->GetRow($_GET["id"]));
+{
+	echo json_encode(FinalizeNetwork($networks->GetRow($_GET["id"])));
+}
 else
-	echo json_encode($networks->All());
+{
+	$all = $networks->All();
+	
+	for($i = 0; $i < count($all); $i++)
+	{
+		$all[$i] = FinalizeNetwork($all[$i]);
+	}
+	
+	echo json_encode($all);
+}
+	
+	
+function FinalizeNetwork($raw)
+{
+	if($raw == null)
+		return null;
+
+	include "database.php";
+	$raw->users = [];
+	$links = $userToNetwork->Where("`networkId`='$raw->id'");
+	
+	for($i = 0; $i < count($links); $i++)
+	{
+		$user = $users->GetRow($links[$i]->userId);
+		$newUser = new stdClass();
+		
+		$newUser->id = $user->id;
+		$newUser->name = $user->name;
+		$newUser->email = $user->email;
+		$newUser->phoneId = $user->phoneId;
+		array_push($raw->users, $newUser);
+	}
+	
+	return $raw;
+}
--- API/users.php
+++ API/users.php
@@ -3,6 +3,38 @@
 include "database.php";
 
 if(isset($_GET["id"]))
-	echo json_encode($users->GetRow($_GET["id"]));
+{
+	echo json_encode(FinalizeChar($users->GetRow($_GET["id"])));
+}
 else
-	echo json_encode($users->All());
+{
+	$all = $users->All();
+	
+	for($i = 0; $i < count($all); $i++)
+	{
+		$all[$i] = FinalizeChar($all[$i]);
+	}
+	
+	echo json_encode($all);
+}
+	
+	
+function FinalizeChar($raw)
+{
+	if($raw == null)
+		return null;
+
+	include "database.php";
+	$raw->prefs = json_decode($raw->prefs);
+	$raw->networks = [];
+	unset($raw->password);// = "****";
+	$links = $userToNetwork->Where("`userId`='$raw->id'");
+	
+	for($i = 0; $i < count($links); $i++)
+	{
+		$network = $networks->GetRow($links[$i]->networkId);
+		array_push($raw->networks, $network);
+	}
+	
+	return $raw;
+}
